55c4003c5dc9e73921610fe7213708b74249bc31
Arthur Battut <arthur@soixantecircuits.fr>
2015-05-23 16:35:45 +0200

    improve glitch methods

--- js/main.js
+++ js/main.js
@@ -13,12 +13,7 @@ function printLine(className, containerID, title, data){
       html:'<br/><h2>$ > '+ title +' : --</h2>'+'<p>'+JSON.stringify(data)+'</p>',
       'class':className + ' line'
     }).appendTo($container);
-    var randomNumber = getRandomInt(0,2);
-    var needToGlitch = (randomNumber == 1)? true : false;
-    if(needToGlitch){
-      glitchTitle($content.find('h2'));
-      glitchPhrase($content.find('p'));
-    }
+    glitchTypo($content);
     godown($container, className);
 }
 
@@ -70,12 +65,7 @@ function printLine(className, containerID, title, data){
     }).appendTo($container);
     $('#people').html(data.numPeople);
     godown($container, className);
-    var randomNumber = getRandomInt(0,2);
-    var needToGlitch = (randomNumber == 1)? true : false;
-    if(needToGlitch){
-      glitchTitle($content.find('h2'));
-      glitchPhrase($content.find('p'));
-    }
+    glitchTypo($content);
   })
 
   function godown($el, className){
@@ -89,6 +79,15 @@ function printLine(className, containerID, title, data){
   }
 
 var typoClasses = ['daedra', 'rune', 'etro', 'harpers'];
+
+function glitchTypo($content){
+  var randomNumber = getRandomInt(0,2);
+  var needToGlitch = (randomNumber == 1)? true : false;
+  if(needToGlitch){
+    glitchTitle($content.find('h2'));
+    glitchPhrase($content.find('p'));
+  }
+}
 
 function glitchTitle($title){
   if($title.children('span').length == 0){
